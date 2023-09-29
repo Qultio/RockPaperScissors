90b7654c7bb0d00735a6628039b1174e1b8fae80
Benjamin <Benjaminneyt@gmail.com>
2023-09-29 20:45:58 +0200

    Get Random Computer Choice

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Connected!")
+function getComputerChoice() {
+    let RanNum = Math.floor(Math.random() * 3);
+    let result;
+    if (RanNum === 0) {
+        result = "Rock";
+    } else if (RanNum === 1) {
+        result = "Paper";
+    } else {
+        result = "Scissors";
+    }
+
+    return result;
+}
+
+console.log(getComputerChoice());
